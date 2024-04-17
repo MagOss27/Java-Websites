d87434219a0d87819350135bd7e8d913b097b91c
MAGNUS OSS <magnus_oss@estudante.sesisenai.org.br>
2024-04-17 15:31:35 -0300

    Criação do first_Java

---
+++ first_Java/src/App.java
@@ -0,0 +1,8 @@
+// Declaração da classe chamada App.
+public class App {
+    // O método principal do programa Java, que é o ponto de entrada quando o programa é executado.
+    public static void main(String[] args) throws Exception {
+        // Imprime a mensagem "Hello, World!" no console.
+        System.out.println("Bem Vindo ao Primeiro Projeto em Java :)");
+    }
+}
